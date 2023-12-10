c122abfb378f643863487a71bb07ffece08a55f3
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-10 19:20:26 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "pv24-terraform-state"
+    key    = "parameter-store/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "parameters" {
+  default = [
+    {name = "test" , type = String, value = "test"},
+    {name = "test" , type = String, value = "test1"}
+  ]
+}
+
+resource "aws_ssm_parameter" "params" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
