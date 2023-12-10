e7a890b1fb41f3a59c6e4876f5901c40bc78ac5c
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-10 19:27:55 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
 variable "parameters" {
   default = [
-    {name = "test" , type = String, value = "test"},
-    {name = "test1" , type = String, value = "test1"}
+    {name = "test" , type = string, value = "test"},
+    {name = "test1" , type = string, value = "test1"}
   ]
 }
 
