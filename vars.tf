9f6b04a83bd5972f042844f126e238a65afced77
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-14 08:39:19 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars.tf
+++ vars.tf
@@ -3,6 +3,8 @@ variable "parameters" {
     {name = "dev.rds.username" , type = "String", value = "admin1"},
     {name = "dev.backend.DB_HOST" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
 
+    {name = "dev.rds.endpoint" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
+    {name = "dev.rds.username" , type = "String", value = "admin1"},
 
     #PASSWORDS --- It should not kept in code, Always for password we create mostly manually
     {name = "dev.rds.password" , type = "SecureString", value = "ExpenseApp1234"}
