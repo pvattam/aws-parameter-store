72a824f03dd1ddc7d532bd89619c18c488a59b4a
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-10 23:16:47 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -11,4 +11,5 @@ resource "aws_ssm_parameter" "params" {
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
+  overwrite = true
 }
--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "parameters" {
   default = [
     {name = "dev.backend.DB_HOST" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
     {name = "dev.frontend.BACKEND_ENDPOINT" , type = "String", value = "http://backend-dev.devopspv24.online/"},
+    {name = "ssh.username", type = "String", value = "root"},
 
     {name = "dev.rds.endpoint" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
     {name = "dev.rds.username" , type = "String", value = "admin1"},
@@ -10,5 +11,6 @@ variable "parameters" {
     {name = "dev.rds.password" , type = "SecureString", value = "ExpenseApp1234"},
     {name = "sonar.token" , type = "SecureString", value = "4f600c624d928cf4cce64c5e940438759ba67f80"},
     {name = "artifactory.password" , type = "SecureString", value = "Admin123"},
+    {name = "ssh.password" , type = "SecureString", value = "DevOps321"},
   ]
 }
