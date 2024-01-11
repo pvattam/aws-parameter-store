bfada0b370b4b12b9d2ffd619e1ffa988965dd57
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-11 07:42:12 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars.tf
+++ vars.tf
@@ -3,6 +3,7 @@ variable "parameters" {
     {name = "dev.backend.DB_HOST" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
     {name = "dev.frontend.BACKEND_ENDPOINT" , type = "String", value = "http://backend-dev.devopspv24.online/"},
     {name = "ssh.username", type = "String", value = "root"},
+    {name = "artifactory.username", type = "String", value = "admin"},
 
     {name = "dev.rds.endpoint" , type = "String", value = "dev-mysql-rds.carn17scsnwv.us-east-1.rds.amazonaws.com"},
     {name = "dev.rds.username" , type = "String", value = "admin1"},
